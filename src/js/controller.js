28b79a709417729214d7cb579c3a5a110ab1b32f
Aleksandarns1989 <Aleksandarns1989@gmail.com>
2024-02-25 13:24:43 +0100

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -135,6 +135,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function(){
+  console.log('Welcome to the application!');
+}
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controleRecipes);
@@ -143,6 +147,6 @@ const init = function () {
   searchView.addHandlerSearch(controleSearchResults);
   paginationView.addHandlerClick(controlePagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome!')
+  newFeature();
 };
 init();
